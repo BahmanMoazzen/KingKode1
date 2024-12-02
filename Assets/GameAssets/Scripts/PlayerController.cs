e9d9eaa93667d1e7d6a2d4547fc41b4ddd6b9ef3
Bahman Moazzen <bahmanmoazzen@gmail.com>
2024-12-02 09:08:13 +0330

    AI Nav Input System

---
+++ Assets/GameAssets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController _controller;
+    [SerializeField] float _moveSpeed;
+    [SerializeField] float _turnSpeed;
+    [SerializeField] Vector3 _turnAxis = new Vector3(0, 1, 0);
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    public void _MoveForward(float iForwardFactor)
+    {
+        _controller.Move(transform.forward * _moveSpeed * iForwardFactor);
+    }
+    public void _Turn(float iTurnFactor)
+    {
+        transform.Rotate(_turnAxis * iTurnFactor * _turnSpeed);
+    }
+}
--- Assets/GameAssets/Scripts/PlayerInputController.cs
+++ Assets/GameAssets/Scripts/PlayerInputController.cs
@@ -5,11 +5,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputController : MonoBehaviour
 {
+    PlayerController _controller;
+    [SerializeField] InputActionReference _move;
+    
+    private void Awake()
+    {
+        _controller = GetComponent<PlayerController>();
+    }
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
     public void MoveOn(InputAction.CallbackContext iContext)
     {
         if (iContext.started)
         {
-            Debug.Log("test");
+            Vector2 moveFactor  = _move.action.ReadValue<Vector2>();
+            _controller._MoveForward(moveFactor.x);
+            _controller._Turn(moveFactor.y);
 
         }
 
