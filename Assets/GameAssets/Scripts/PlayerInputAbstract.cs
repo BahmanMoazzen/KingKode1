c03780ae1b9abcfb89ebe6e912502e782be22c73
Helen Game Factory <helengamefactory@gmail.com>
2024-12-03 01:10:48 +0330

    Final Job
    All Done

--- Assets/GameAssets/Scripts/PlayerInputAbstract.cs
+++ Assets/GameAssets/Scripts/PlayerInputAbstract.cs
@@ -12,19 +12,9 @@ public abstract class PlayerInputAbstract : MonoBehaviour
     {
         _input = new GameInputs();
         _controller = GetComponent<PlayerController>();
-        _input.UI.Enable();
-    }
-    protected virtual void OnEnable()
-    {
         _input.Player.Enable();
-        
-
     }
-
-    protected virtual void OnDisable()
-    {
-        _input.Player.Disable();
-    }
+    
     protected abstract void _DoInput();
     private void Update()
     {
--- Assets/GameAssets/Scripts/PlayerPointAndClickInput.cs
+++ Assets/GameAssets/Scripts/PlayerPointAndClickInput.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
@@ -16,19 +14,16 @@ public class PlayerPointAndClickInput : PlayerInputAbstract
         _mainCam = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
     }
-    protected override void OnEnable()
+    protected void OnEnable()
     {
-        base.OnEnable();
         _input.Player.MouseClicked.performed += MouseClicked_performed;
     }
-    protected override void OnDisable()
+    protected void OnDisable()
     {
-        base.OnDisable();
         _input.Player.MouseClicked.performed -= MouseClicked_performed;
     }
     private void MouseClicked_performed(InputAction.CallbackContext obj)
     {
-        Debug.Log("Mouse Clicked");
         RaycastHit hitInfo;
         Physics.Raycast(_mainCam.ScreenPointToRay(Mouse.current.position.ReadValue())
             ,out hitInfo, _maxDistance, _groundMask);
